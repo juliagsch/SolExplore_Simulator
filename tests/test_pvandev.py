f854984f1ad72b142c1a1f3bccd2b4ba24e5d0e5
juliagsch <julia.gschwind@hotmail.com>
2025-03-28 09:47:02 +0000

    dummy tests

--- tests/test_pvandev.py
+++ tests/test_pvandev.py
@@ -70,6 +70,26 @@ def test_bidirectional_two_trips():
     assert result["total_cost"] == pytest.approx((result["ev_power_charged"]-5.333333)*0.07 + 3*0.35, 1e-6) # We can always cover the household load from the EV or PV except during 3h (1h before sunrise and 2h after sunset as the EV is away or charging)
     assert result["power_lost"] == pytest.approx((result["ev_power_charged"])*(1-eta_ev)+(11*((1.0 / eta_ev)-1)), 1e-6) # We discharge 11 kWh from the EV battery
 
+def test_bidirectional_two_trips_no_load():
+    house_file_path = f"{test_data_path}/house0.txt"
+    solar_file_path = f"{test_data_path}/solar1.txt"
+    ev_file_path = f"{test_data_path}/ev2.csv"
+    op = "hybrid_bidirectional"
+    num_days = 1
+    command = f"./bin_pvandev/sim 2100 480 10 20 1 0.5 0.95 {num_days} {house_file_path} {solar_file_path} 0.8 0.2 60.0 7.4 {op} {ev_file_path} 0 4"
+    result = subprocess.run(command.split(), stdout=subprocess.PIPE, text=True)
+    print(command)
+    # Extract numbers from output
+    result = extract_result(result.stdout)
+    
+    assert result["ev_power_used"] == 6
+    assert result["household_load"] == 0
+    assert result["total_hours"] == 24
+    assert result["total_load"] == result["ev_power_charged"]
+    # Addition can lead to small rounding error, therefore we need to approximate.
+    assert result["total_load"] == pytest.approx(result["household_load"]+result["ev_power_used"]+result["power_lost"]+result["ev_battery_diff"], 1e-6)
+    assert result["power_lost"] == pytest.approx((result["ev_power_charged"])*(1-eta_ev), 1e-6) # We discharge 11 kWh from the EV battery
+
 def test_uni_vs_bi():
     house_file_path = f"{test_data_path}/house1.txt"
     solar_file_path = f"{test_data_path}/solar1.txt"
@@ -117,3 +137,26 @@ def test_pv_vs_nopv():
     assert result_nopv["household_load"] == result_pv["household_load"]
     assert result_nopv["grid_import"] > result_pv["grid_import"]
     assert result_nopv["power_lost"] > result_pv["power_lost"] # We are discharging more without pv
+
+def test_pv_vs_nopv_zerosolar():
+    house_file_path = f"{test_data_path}/house1.txt"
+    solar_file_path = f"{test_data_path}/solar0.txt"
+    ev_file_path = f"{test_data_path}/ev1.csv"
+    op_bi = "hybrid_bidirectional"
+    num_days = 1
+    command_nopv = f"./bin_nopvbutev/sim 2100 480 10 20 1 0.5 0.95 {num_days} {house_file_path} {solar_file_path} 0.8 0.2 60.0 7.4 {op_bi} {ev_file_path} 0 4"
+    command_pv = f"./bin_pvandev/sim 2100 480 10 20 1 0.5 0.95 {num_days} {house_file_path} {solar_file_path} 0.8 0.2 60.0 7.4 {op_bi} {ev_file_path} 0 4"
+
+    result_nopv = subprocess.run(command_nopv.split(), stdout=subprocess.PIPE, text=True)
+    result_pv = subprocess.run(command_pv.split(), stdout=subprocess.PIPE, text=True)
+
+    # Extract numbers from output
+    result_nopv = extract_result(result_nopv.stdout)
+    result_pv = extract_result(result_pv.stdout)
+
+    assert result_nopv["ev_power_used"] == 2
+    assert result_nopv["household_load"] == 24
+    assert result_nopv["ev_power_used"] == result_pv["ev_power_used"]
+    assert result_nopv["household_load"] == result_pv["household_load"]
+    assert result_nopv["grid_import"] == result_pv["grid_import"]
+    assert result_nopv["power_lost"] == result_pv["power_lost"]
